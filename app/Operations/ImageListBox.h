44350450d60cebdc49fb26d7e8f7d54be504b8ca
Sacha Percot-Tétu <zakinster@zakinster.com>
2012-07-17 13:03:37 +0200

    Added the MixImgListBox to handle std/dbl images

--- app/Operations/ImageListBox.h
+++ app/Operations/ImageListBox.h
@@ -56,4 +56,56 @@ protected:
 
 typedef ImageListBox_t<imagein::Image::depth_t> ImageListBox;
 
+class MixImageListBox : public QComboBox
+{
+public:
+    enum ImageType {STDIMG, DBLIMG};
+    explicit MixImageListBox(QWidget *parent,
+                          std::string currentImg,
+                          std::map<const imagein::Image*,std::string> stdImgs,
+                          std::map<const imagein::Image_t<double>*,std::string> dblImgs)
+        : QComboBox(parent)
+    {
+        int i = 0, index = 0;
+        for(typename std::map<const imagein::Image*, std::string>::const_iterator it = stdImgs.begin(); it != stdImgs.end(); ++it) {
+            _images.insert(std::pair<std::string, ImageType>(it->second, STDIMG));
+            _stdImgs.insert(std::pair<std::string, const imagein::Image*>(it->second, it->first));
+            this->insertItem(i, QString(it->second.c_str()));
+            if(it->second == currentImg) index = i;
+        }
+        for(typename std::map<const imagein::Image_t<double>*, std::string>::const_iterator it = dblImgs.begin(); it != dblImgs.end(); ++it) {
+            _images.insert(std::pair<std::string, ImageType>(it->second, DBLIMG));
+            _dblImgs.insert(std::pair<std::string, const imagein::Image_t<double>*>(it->second, it->first));
+            this->insertItem(i, QString(it->second.c_str()));
+            if(it->second == currentImg) index = i;
+        }
+        this->setCurrentIndex(index);
+    }
+
+    ImageType currentType() {
+        std::string name = this->currentText().toStdString();
+        return _images[name];
+    }
+
+    const imagein::Image* getStdImage(std::string name) {
+        typename std::map<std::string, const imagein::Image*>::iterator it = _stdImgs.find(name);
+        if(it != _stdImgs.end()) {
+            return _stdImgs[name];
+        }
+        return NULL;
+    }
+
+    const imagein::Image_t<double>* getDblImage(std::string name) {
+        typename std::map<std::string, const imagein::Image_t<double>*>::iterator it = _dblImgs.find(name);
+        if(it != _dblImgs.end()) {
+            return _dblImgs[name];
+        }
+        return NULL;
+    }
+protected:
+    std::map<std::string, ImageType> _images;
+    std::map<std::string, const imagein::Image*> _stdImgs;
+    std::map<std::string, const imagein::Image_t<double>*> _dblImgs;
+
+};
 #endif // IMAGELISTBOX_H
