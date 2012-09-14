5719f89d6776aefa4a26a6c6be8443ec5303548a
Sacha Percot-Tétu <zakinster@zakinster.com>
2012-09-14 14:20:47 +0200

    Added real colorimetry operation

---
+++ app/Operations/ColorDialog.cpp
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2011-2012 INSA Rennes
+ *
+ * This file is part of EIImage.
+ *
+ * EIImage is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * EIImage is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with EIImage.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "ColorDialog.h"
+#include "ui_ColorDialog.h"
+
+ColorDialog::ColorDialog(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::ColorDialog)
+{
+    ui->setupUi(this);
+    ui->hsvWidget->setVisible(false);
+    this->adjustSize();
+}
+
+ColorDialog::~ColorDialog()
+{
+    delete ui;
+}
+
+QColor ColorDialog::getColor() const {
+
+    if(ui->rgbButton->isChecked()) {
+        int r = ui->redBox->value();
+        int g = ui->greenBox->value();
+        int b = ui->blueBox->value();
+        return QColor::fromRgb(r, g, b);
+    }
+    else {
+        int h = ui->hueBox->value();
+        int s = ui->satBox->value();
+        int v = ui->valBox->value();
+        return QColor::fromHsv(h, s, v);
+    }
+}
