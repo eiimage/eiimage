b1fddbba2960490cdf83a564978cfdb8fb497188
Sacha Percot-Tétu <zakinster@zakinster.com>
2012-09-25 18:07:28 +0200

    Removed randomlib from eiimage

--- app/RandomLib/Config.h
+++
@@ -1,9 +0,0 @@
-// $Id: e8cf4f146baf9674fb19041fe3f42231958e0200 $
-
-#define RANDOMLIB_VERSION_STRING "1.5"
-
-// These are macros which affect the building of the library
-#define RANDOM_SHARED_LIB 1
-/* #undef HAVE_SSE2 */
-/* #undef HAVE_ALTIVEC */
-#define HAVE_LONG_DOUBLE 1
