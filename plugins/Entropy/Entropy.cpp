7abf2807659a0a8ab17032e51de1c432bc86031c
Antoine Lorence <antoine.lorence@insa-rennes.fr>
2014-11-18 13:23:26 +0100

    [plugins] Add missing plugins files

---
+++ plugins/Entropy/Entropy.cpp
@@ -0,0 +1,129 @@
+/*
+ * Copyright 2011-2012 INSA Rennes
+ *
+ * This file is part of EIImage.
+ *
+ * EIImage is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * EIImage is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with EIImage.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <iostream>
+#include <string>
+
+#include "Image.h"
+#include "Converter.h"
+
+#include "Operation.h"
+#include "Plugin.h"
+#include "ImgParam.h"
+#include "IntParam.h"
+#include "PlugOperation.h"
+
+#include <cstring>
+#include <cstdio>
+#include <cmath>
+
+using namespace std;
+using namespace imagein;
+
+
+class EntropyOp : public PlugOperation {
+  public:
+    EntropyOp() : PlugOperation("Entropy") {
+        addParam(CurrentImg(), &EntropyOp::img);
+//        addParam(IntParam("Entier", 0, 255, 127), &Entropy::threshold);
+        //this->addParam(ImgParam("Image"), &Entropy::img);
+    }
+
+    void operation() {
+
+        double entropy = 0.;
+
+        // for each image channel
+        for(unsigned int c = 0; c < img.getNbChannels(); ++c) {
+            Histogram histo = img.getHistogram(c);
+
+            // A MODIFIER !!!!!
+            // histo est un tableau
+            //img.getWidth() et img.getHeight() peuvent être pratiques pour trouver les dimensions de l'image...
+
+        }
+        entropy = - entropy / log(2);
+
+    //        this->outImage(result);
+        char buffer[255];
+        sprintf( buffer, "Entropy of the image = %.2f bpp", entropy );
+        string s = buffer ;
+        outText(s);
+    }
+  private:
+    Image img;
+};
+
+extern "C" Plugin* loadPlugin() {
+    Plugin* plugin = new Plugin("Entropy");
+    plugin->addOperation(new EntropyOp());
+    return plugin;
+}
+
+extern "C" void unloadPlugin(Plugin* plugin) {
+    delete plugin;
+}
+
+//
+//extern "C" Plugin* getPlugin() {
+//    Plugin* myPlugin = new MyPlugin("ImageRef");
+//    return myPlugin;
+//}
+//
+//class MyOperation : public Operation {
+//  public:
+//    MyOperation(string name) : Operation(name) {
+//        //this->addParam(ImgParam("image"), &MyOperation::image);
+//        //this->addParam(CurrentImg(), &MyOperation::img);
+//        //this->addParam(IntParam("Entier", 0, 255, 127), &MyOperation::n);
+//    }
+//
+//    void operation() {
+//        cout << "n = " << n << endl;
+//
+//        Image* res = generateRefImg();
+//
+//        this->addOutput(ImgOutput("ImageRef", *res));
+//    }
+//  private:
+//    int n;
+//    Image img;
+//};
+//
+//class MyPlugin : public Plugin {
+//  public:
+//    MyPlugin(string name) : Plugin(name) {
+//        try {
+//            MyOperation* op = new MyOperation("Générer image");
+//            this->addOperation(op);
+//            //this->addOperation(new Identity("Identité"));
+//            /*MyOperation* op2 = new MyOperation("Dilatation");
+//            this->addOperation(op2);*/
+//        }
+//        catch(const char* msg) {
+//            std::cerr << msg << std::endl;
+//        }
+//    }
+//};
+//
+//
+//extern "C" Plugin* getPlugin() {
+//    Plugin* myPlugin = new MyPlugin("ImageRef");
+//    return myPlugin;
+//}
---
+++ plugins/SegmentationTest/segmentation.cpp
@@ -0,0 +1,101 @@
+/*
+ * Copyright 2011-2012 INSA Rennes
+ *
+ * This file is part of EIImage.
+ *
+ * EIImage is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * EIImage is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with EIImage.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <iostream>
+#include <string>
+
+#include "Image.h"
+#include "Algorithm/Binarization.h"
+#include "Algorithm/Otsu.h"
+#include "Algorithm/Dithering.h"
+#include "Converter.h"
+
+#include "Operation.h"
+#include "Plugin.h"
+#include "ImgParam.h"
+#include "IntParam.h"
+
+#include "Input.h"
+#include "Parameter.h"
+
+using namespace std;
+using namespace imagein;
+
+class Thresholding : public PlugOperation {
+  public:
+    Thresholding() : PlugOperation("Thresholding") {
+        addParam(CurrentImg(), &Thresholding::img);
+        addParam(IntParam("Entier", 0, 255, 127), &Thresholding::threshold);
+    }
+
+    void operation() {
+        algorithm::Binarization_t<depth_default_t> thresholding(threshold);
+        GrayscaleImage* grayImg = imagein::Converter<GrayscaleImage>::convert(img);
+        GrayscaleImage* result = thresholding(grayImg);
+        outImage(result);
+    }
+  private:
+    int threshold;
+    Image img;
+};
+
+class Otsu : public PlugOperation {
+  public:
+    Otsu() : PlugOperation("Otsu") {
+        this->addParam(CurrentImg(), &Otsu::img);
+    }
+
+    void operation() {
+        algorithm::Otsu_t<depth_default_t> otsu;
+        GrayscaleImage* grayImg = imagein::Converter<GrayscaleImage>::convert(img);
+        GrayscaleImage* result = otsu(grayImg);
+        outImage(result);
+    }
+  private:
+    Image img;
+};
+
+class Dithering : public PlugOperation {
+  public:
+    Dithering() : PlugOperation("Dithering") {
+       this->addParam(CurrentImg(), &Dithering::img);
+//        this->addParam(ImgParam("Image"), &Dithering::img);
+    }
+
+    void operation() {
+        algorithm::Dithering_t<depth_default_t> dithering;
+        Image* result = dithering(&img);
+        this->outImage(result);
+        this->outText("test");
+    }
+  private:
+    Image img;
+};
+
+extern "C" Plugin* loadPlugin() {
+    Plugin* plugin = new Plugin("Binarization");
+    plugin->addOperation(new Thresholding());
+    plugin->addOperation(new Otsu());
+    plugin->addOperation(new Dithering());
+    return plugin;
+}
+
+extern "C" void unloadPlugin(Plugin* plugin) {
+    delete plugin;
+}
